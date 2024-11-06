d5cb331465d65582052a71eb2c30a848044eca16
md-hafizur <mdhafizur.cse@gmail.com>
2024-11-06 21:07:03 +0600

    add rediologist model

--- apps/account/models.py
+++ apps/account/models.py
@@ -21,7 +21,7 @@ class Role(BaseModel):
 
     def __str__(self):
         return f"{self.name} ({self.label})"
-    
+
 
 class Department(BaseModel):
     label = models.CharField(max_length=100, unique=True, blank=False)
@@ -59,3 +59,25 @@ class User(AbstractUser, BaseModel):
     USERNAME_FIELD = "phone"
     REQUIRED_FIELDS = ["email"]
     USERNAME_FIELD = "phone"
+
+
+class Rediologist(BaseModel):
+    phone = PhoneNumberField(unique=True, db_index=True, verbose_name="Phone Number")
+    email = models.EmailField(unique=True, db_index=True, max_length=50)
+    avatar = models.ImageField(
+        "Avatar",
+        upload_to="avatars",
+        null=True,
+        blank=True,
+    )
+    status = models.CharField(
+        max_length=20,
+        choices=UserStatus.choices,
+        default=UserStatus.ACTIVE,
+    )
+    department = models.ManyToManyField(
+        Department, related_name="rediologists", blank=True
+    )
+
+    def __str__(self):
+        return f"{self.phone}"
