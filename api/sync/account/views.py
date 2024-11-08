6e33cbdaea7a8bf1961ef030f5d64293cb60338d
mac <mac@macs-Mac-mini.local>
2024-11-09 00:58:37 +0600

    update User details api

--- api/sync/account/views.py
+++ api/sync/account/views.py
@@ -1,21 +1,36 @@
 from restapi.views.crud import CrudAPIView
 from apps.account.models import User
 from .serializers import UserSerializer
+from django.db.models import Q
+from restapi.response import DictResponse
 
 
-class radiologist(CrudAPIView):
+class BaseUserhView(CrudAPIView):
+    serializer_class = UserSerializer
+    lookup_field = "id"
+
+    def get(self, request, *args, **kwargs):
+        keyword = self.request.GET.get("keyword")
+        if keyword:
+            self.queryset = self.queryset.filter(
+                Q(email__icontains=keyword)
+                | Q(first_name__icontains=keyword)
+                | Q(phone__icontains=keyword)
+            )
+
+        serializer = self.get_serializer(self.queryset, many=True)
+        data = serializer.data
+
+        return DictResponse(data=data, status=200, message="successful", safe=False)
+
+
+class radiologist(BaseUserhView):
     queryset = User.objects.filter(isRadiologist=True)
-    serializer_class = UserSerializer
-    lookup_field = "id"
 
 
-class admin(CrudAPIView):
-    queryset = User.objects.filter(isAdmin=True)
-    serializer_class = UserSerializer
-    lookup_field = "id"
-
-
-class radiographer(CrudAPIView):
+class radiographer(BaseUserhView):
     queryset = User.objects.filter(isRadiographer=True)
-    serializer_class = UserSerializer
-    lookup_field = "id"
+
+
+class admin(BaseUserhView):
+    queryset = User.objects.prefetch_related("role").filter(isAdmin=True)
