798db934deef442d71b8bf85d11369a206beb2bb
Md Imran Hossain <58961202+mdimranh@users.noreply.github.com>
2024-11-08 20:40:37 +0600

    .

--- apps/account/admin.py
+++ apps/account/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import User, Role, Department, Rediologist
+from .models import User, Role, Department, Radiologist
 
 admin.site.register(User)
 admin.site.register(Role)
 admin.site.register(Department)
-admin.site.register(Rediologist)
+admin.site.register(Radiologist)
